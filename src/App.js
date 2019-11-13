2435d1971d660d8ed4d85c2f65b2dce3715c47a2
cruel32 <cruel32@nate.com>
2019-11-13 18:25:33 +0900

    redux 미들웨어 thunk 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import 'styles/App.scss';
+import LoginPage from 'containers/login'
 
 function App() {
   return (
     <div className="App">
+      <LoginPage></LoginPage>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import rootReducer from 'store/modules'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 import App from './App';
+import 'styles/index.scss';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
