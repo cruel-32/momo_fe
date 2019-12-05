5baa1ab5111db106e11353f41b6e310a65cea60d
cruel32 <cruel32@nate.com>
2019-12-05 17:23:42 +0900

    add swiper

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
-import 'styles/App.scss';
 import LoginPage from 'containers/login'
 import AccountDetail from 'containers/accountDetail'
 import AccountList from 'containers/accountList'
 import { Button } from '@material-ui/core';
 
+import { TinySwiper } from 'tiny-swiper-react'
+
+import 'styles/App.scss';
+import img1 from 'images/1.png';
+import img2 from 'images/2.png';
+import img3 from 'images/3.png';
+
+
+
 function App() {
   const [mode, setMode] = useState('on');
+
+  const [ items ] = useState([
+    <img src={img1} className="App-logo" alt="logo" />,
+    <img src={img2} className="App-logo" alt="logo" />,
+    <img src={img3} className="App-logo" alt="logo" />,
+    <div>jsx 1</div>,
+    <div>jsx 2</div>,
+    <div>jsx 3</div>,
+  ])
 
   return (
     <BrowserRouter>
       <div className="App">
         <div>
+          <TinySwiper
+            items={items}
+            useIndicator={true}
+            useDirector={true}
+            indicatorClass={"override"}
+            directorLeftClass={"override"}
+            directorRightClass={"override"}
+          >
+          </TinySwiper>
           <Link to="/">home</Link>&nbsp;&nbsp;
           <Link to={`/account/${'tester'}`}>AccountDetail</Link>&nbsp;&nbsp;
           <Link to="/account?sort=name">list</Link>&nbsp;&nbsp;
