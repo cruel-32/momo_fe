1f95000f0df2c65cec1eeb90fdb4861c216a37bf
cruel-32 <cruel32@nate.com>
2019-12-09 22:46:35 +0900

    SideBtnWrap 구현

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -6,24 +6,26 @@ import btnBell from 'images/side-menu/btn_bell.svg'
 export const SideMenu = () =>{
     return (
         <div className="side-menu side-menu--side-left">
-            <SideLoggedAccount></SideLoggedAccount>
-            <SideTogether></SideTogether>
+            <SideAccountMenu></SideAccountMenu>
+            <SideClubMenu></SideClubMenu>
         </div>
     )
 }
 
-export const SideLoggedAccount = ()=>{
+export const SideAccountMenu = ()=>{
     return (
-        <div className="side-menu__side-logged-account side-menu__side-logged-account--active">
-            <img src={btnSet} alt="set" />
-            <img src={btnBell} alt="bell" />
+        <div className="side-menu__side-account-menu side-menu__side-account-menu--active">
+            <div className="side-menu__side-btn-wrap side-menu__side-btn-wrap--pd">
+                <button className="side-menu__side-btn side-menu__side-btn--set "><img className="side-menu__side-btn-img" src={btnSet} alt="set" /></button>
+                <button className="side-menu__side-btn side-menu__side-btn--bell "><img className="side-menu__side-btn-img" src={btnBell} alt="bell" /></button>
+            </div>
 
 
         </div>
     )
 }
 
-export const SideTogether = ()=>{
+export const SideClubMenu = ()=>{
     return (
         <div className="side-menu__side-club">
 
