2561bc5aad6581e5187937dcd694f2060007c189
cruel32 <cshee8508@gmail.com>
2020-01-12 22:25:48 +0900

    infinitScroll 구현중

--- src/components/InfinitScroll/InfinitScroll.js
+++ src/components/InfinitScroll/InfinitScroll.js
@@ -21,13 +21,13 @@ export const InfinitScroll = props =>{
             <div className="infinit-scroll__gradient infinit-scroll__gradient--pos-top"
                 style={{
 
-                    "background" : `linear-gradient(to bottom, ${gradientColor} 0%,rgba(255,255,255,0) 100%)`
+                    "background" : `linear-gradient(to bottom, ${gradientColor || '#fff'} 0%,rgba(255,255,255,0) 100%)`
                 }}
             ></div>
             {children}
             <div className="infinit-scroll__gradient infinit-scroll__gradient--pos-bot"
                 style={{
-                    "background" : `linear-gradient(to top, ${gradientColor} 0%,rgba(255,255,255,0) 100%)`
+                    "background" : `linear-gradient(to top, ${gradientColor || '#fff'} 0%,rgba(255,255,255,0) 100%)`
                 }}
             ></div>
         </div>
